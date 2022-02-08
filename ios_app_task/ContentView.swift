eb6f1fdd8b1ebafcb3677e28ee92ef23fc2526a3
Syed Raza Haider <raxa.hai@gmail.com>
2022-02-08 18:36:08 +0500

    Initial Commit

---
+++ ios_app_task/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_app_task
+//
+//  Created by Syed Raza Haider on 08/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios_app_task/ios_app_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_app_taskApp.swift
+//  ios_app_task
+//
+//  Created by Syed Raza Haider on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ios_app_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
